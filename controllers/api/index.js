dd3be9445184ac1d8caa9658dfeba8a9c5146609
Felix Gutierrez <fgutierrez2191@icloud.com>
2021-01-13 12:20:45 -0500

    created login and signup page

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,7 +3,6 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-const { route } = require('./post-routes');
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
